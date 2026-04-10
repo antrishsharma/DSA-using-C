a2eb8f5dc45011bb15357202e8c457fb52f485f0
ANTRISH SHARMA <antrishsharma555@gmail.com>
2026-04-10 23:54:08 +0530

    Implement list creation and display using arrays
    Create a program that takes 'n' elements as input and displays them.

---
+++ List using array 01.c
@@ -0,0 +1,17 @@
+//1.Create a list of n elements and display them.
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0; i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    
+    
+    for(int i=0; i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    
+return 0;
+}
